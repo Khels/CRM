45fc21f2b180e3f2388cfd56175630346076ad04
Denis Mahnyuk <denismahnuk@gmail.com>
2022-11-24 18:37:16 +0300

    init: backend structure

---
+++ backend/src/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+from .config import DATABASE_URL, DEBUG
+
+engine = create_async_engine(DATABASE_URL, future=True, echo=DEBUG)
+
+Base = declarative_base()
+
+async_session = sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+
+# dependency
+async def get_db_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
