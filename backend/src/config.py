9f331e86e25b048971a0d2d26835a749c73d7fee
Denis Mahnyuk <denismahnuk@gmail.com>
2022-11-25 11:57:09 +0300

    feature(backend): set up cors

--- backend/src/config.py
+++ backend/src/config.py
@@ -9,6 +9,9 @@ DEBUG = config["common"]["debug"]
 ACCESS_TOKEN_EXPIRE_MINUTES = 30000
 REFRESH_TOKEN_EXPIRE_DAYS = 30 * 6
 
+# CORS
+ALLOWED_ORIGINS = config["cors"]["allowed_origins"]
+
 # database
 DATABASE_HOST = config["database"]["host"]
 DATABASE_PORT = config["database"]["port"]
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .auth.router import router as auth_router
+from .config import ALLOWED_ORIGINS
 
 app = FastAPI(
     title="CMR for HRs",
     description="Welcome to CMR's API documentation! Here you will be able to discover all of the ways you can interact with the CMR API.",  # noqa: E501
     version="0.1.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_router)
