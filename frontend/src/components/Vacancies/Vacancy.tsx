13aa33528e7aab1faac5d52a071f9d395473a82c
nazaret <nazaret155@mail.ru>
2022-11-26 00:20:43 +0300

    add vacancy deleting function; some fixes for vacancy create modal

--- frontend/src/components/Vacancies/Vacancy.tsx
+++ frontend/src/components/Vacancies/Vacancy.tsx
@@ -1,14 +1,21 @@
 import { ListItem, ListItemProps, ListItemText } from "@mui/material";
 import { FC } from "react";
+import { vacancyStore } from "../../store/vacancy";
 
 export interface IVacancyProps extends ListItemProps {
   title: string;
+  id: number;
 }
 
 export const Vacancy: FC<IVacancyProps> = ({ title, ...props }) => {
   return (
     <ListItem {...props}>
       <ListItemText primary={title}></ListItemText>
+      <Tooltip title="Удалить вакансию">
+        <IconButton onClick={removeItem}>
+          <DeleteIcon />
+        </IconButton>
+      </Tooltip>
     </ListItem>
   );
 };
